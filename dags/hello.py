9bfdf865ebfe6103a71283a867b7cd87aa052378
Anupam <iamanupampandit@gmail.com>
2024-12-28 13:25:56 +0530

    Added second task to DAG

--- dags/hello.py
+++ dags/hello.py
@@ -1,12 +1,19 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 
 def print_a():
-    print("Hello Anupam| There is an update")
+    print("Content Updated")
 
 
 with DAG("my_dag", start_date=datetime(2024, 12, 8), schedule='@daily', catchup=False,
          tags=['data_science'], description='A Sample DAG'):
     task_a = PythonOperator(task_id='task_a', python_callable=print_a)
+    task_b = BashOperator(
+        task_id='task_b',
+        bash_command='echo hello from bash'
+    )
+
+    task_b >> task_a
