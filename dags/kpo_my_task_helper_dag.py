d4864a5fd8e924a1711a3f2a7c665412f46d98bb
Anupam Pandit <iamanupampandit@gmail.com>
2025-10-13 15:07:09 +0530

    Update kpo_my_task_helper_dag.py
    added new func

--- dags/kpo_my_task_helper_dag.py
+++ dags/kpo_my_task_helper_dag.py
@@ -20,6 +20,18 @@ with DAG(
         is_delete_operator_pod=True, # Delete the pod after completion
         get_logs=True, # Fetch logs from the pod
         )
+
+    run_csv_string_to_json_func = KubernetesPodOperator(
+        task_id='run_csv_string_to_json_func',
+        namespace='airflow',  # Or your specific Kubernetes namespace
+        image='localhost:5000/kpo_task_helper', # Replace with your image
+        image_pull_policy='Never', # Download local images
+        cmds=['python'],
+        arguments=['/app/my_task_helper.py', 'csv_string_to_json', 'Name,Age,City\nJhon,28,New York\nAlice,24, Los Angeles\nBob,30,Chicago'],
+        name='python-script-with-args',
+        is_delete_operator_pod=True, # Delete the pod after completion
+        get_logs=True, # Fetch logs from the pod
+    )
     
 
-    run_python_script
+    run_python_script >> run_csv_string_to_json_func
