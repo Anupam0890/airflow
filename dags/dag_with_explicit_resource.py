b36b00035d60ad733698b9c19cc950e3c7b8ffb1
Anupam Pandit <iamanupampandit@gmail.com>
2025-10-25 13:29:35 +0530

    Create DAG with explicit resource configuration
    Define a DAG with explicit resource requirements for tasks.

---
+++ dags/dag_with_explicit_resource.py
@@ -0,0 +1,45 @@
+from airflow.decorators import dag, task
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+import time
+from kubernetes.client import models as k8s
+
+k8s_exec_config_resource_requirements = {
+    "pod_override": k8s.V1Pod(
+        spec=k8s.V1PodSpec(
+            containers=[
+                k8s.V1Container(
+                    name="base",
+                    resources=k8s.V1ResourceRequirements(
+                        requests={"cpu": "500m", "memory": "1024Mi"},
+                        limits={"cpu": 1, "memory": "1024Mi"}
+                    )
+                )
+            ]
+        )
+    )
+}
+
+@dag(
+    'dag_with_explicit_resource',
+    start_date=datetime.today() - timedelta(days=1),
+    schedule_interval='@daily',
+    description='A Simple Dag Explicit ',
+    tags=['data_platform'],
+    catchup=False
+)
+def dag_with_explicit_resource():
+    bash_resource_overrise = BashOperator(
+      task_id="bash_resource_requirements_override_example",
+      bash_command="echo hi",
+      executor_config=k8s_exec_config_resource_requirements
+    )
+    @task(executor_config=k8s_exec_config_resource_requirements)
+    def resource_requirements_override_example():
+        print("Started waiting ...")
+        time.sleep(60)
+
+    bash_resource_overrise >> resource_requirements_override_example()
+
+
+dag_with_explicit_resource()
